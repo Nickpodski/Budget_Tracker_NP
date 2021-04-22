d0629338c2bdd9c9149dc9c4ba3695ac393183f0
Nickpodski <Nickpodski@gmail.com>
2021-04-22 13:52:27 -0400

    changed a few things in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT ||3000;
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect(process.env.MONGOD_URI, {
+mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost/budgetDB", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
